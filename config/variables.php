4b372c8cb2f9f36a25595b58f71024dd269e4268
Nikos Ziozas <nziozas@gmail.com>
2024-11-06 10:23:48 +0200

    Colors

--- config/variables.php
+++ config/variables.php
@@ -3,7 +3,7 @@
 return [
   "creatorName" => "Nikos Ziozas",
   "creatorUrl" => "https://themeselection.com",
-  "templateName" => "Evolution Team",
+  "templateName" => "FleetSimple",
   "templateDescription" => "Most Powerful & Comprehensive Bootstrap 5 HTML Admin Dashboard Template built for developers!",
   "templateKeyword" => "dashboard, bootstrap 5 dashboard, bootstrap 5 design, bootstrap 5, bootstrap 5 free, free admin template",
   "licenseUrl" => "https://themeselection.com/license/",
